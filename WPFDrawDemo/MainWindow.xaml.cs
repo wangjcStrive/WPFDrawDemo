4a4a5b6ad6e353e35d564cda1648e52b5f36c000
jicwang <wangjc857@gmail.com>
2020-01-19 17:04:53 +0800

    will add mouse event

--- WPFDrawDemo/MainWindow.xaml.cs
+++ WPFDrawDemo/MainWindow.xaml.cs
@@ -23,6 +23,63 @@ namespace WPFDrawDemo
         public MainWindow()
         {
             InitializeComponent();
+            drawGrid();
+        }
+
+        private void drawGrid()
+        {
+            var centerX = this.Width/2;
+            var centerY = this.Height/2;
+            double diePieth = 30;
+            double R = 100;
+            double dieLen = 0;
+            SolidColorBrush gridBrush = new SolidColorBrush();
+            gridBrush.Color = Colors.Red;
+
+            while (dieLen <= R)
+            {
+                double lineLen = System.Math.Sqrt(R * R - System.Math.Pow(dieLen, 2));
+                Line aa = new Line();
+                aa.X1 = centerX - lineLen;
+                aa.Y1 = centerY + dieLen;
+                aa.X2 = centerX + lineLen;
+                aa.Y2 = centerY + dieLen;
+                aa.Stroke = gridBrush;
+                aa.StrokeThickness = 1;
+                this.myGrid.Children.Add(aa);
+
+                Line bb = new Line();
+                bb.X1 = centerX - lineLen;
+                bb.Y1 = centerY - dieLen;
+                bb.X2 = centerX + lineLen;
+                bb.Y2 = centerY - dieLen;
+                bb.Stroke = gridBrush;
+                bb.StrokeThickness = 1;
+                this.myGrid.Children.Add(bb);
+
+                Line cc = new Line();
+                cc.X1 = centerX - dieLen;
+                cc.Y1 = centerY - lineLen;
+                cc.X2 = centerX - dieLen;
+                cc.Y2 = centerY + lineLen;
+                cc.Stroke = gridBrush;
+                cc.StrokeThickness = 1;
+                this.myGrid.Children.Add(cc);
+
+                Line dd = new Line();
+                dd.X1 = centerX + dieLen;
+                dd.Y1 = centerY - lineLen;
+                dd.X2 = centerX + dieLen;
+                dd.Y2 = centerY + lineLen;
+                dd.Stroke = gridBrush;
+                dd.StrokeThickness = 1;
+                this.myGrid.Children.Add(dd);
+
+
+
+
+                dieLen += diePieth;
+            }
         }
     }
 }
